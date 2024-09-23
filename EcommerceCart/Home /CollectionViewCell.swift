c1cd96b7d2b7b91fb5419739ad470c31b6c76557
BhavyaGovind <bhavyagovind93@gmail.com>
2024-09-24 00:25:34 +0530

    Added collectionView in the home page

---
+++ EcommerceCart/Home /CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  EcommerceCart
+//
+//  Created by Bhavya on 23/09/24.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ EcommerceCart/SectionHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  SectionHeaderView.swift
+//  EcommerceCart
+//
+//  Created by Bhavya on 22/09/24.
+//
+
+import UIKit
+
+class SectionHeaderView: UICollectionReusableView {
+        
+}
